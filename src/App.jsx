8923765b1023cd6d0754078c70a48b772cfeac37
Noor Alam <nooralam192001@gmail.com>
2024-11-26 15:30:49 +0530

    first entry

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Pokemon } from "./Pokemon"
+
+export const App = () => { // when we write export only then we have to access as object in {}, but if we write export default then we can access it by not using {}
+  return (
+    
+      <Pokemon/>
+    
+  )
+}
